6a0a55c1320cc6bbbd7f5ac6e2deeb1925cb90e6
HI Po <prantokih42@gmail.com>
2024-05-12 02:39:11 +0600

    register part added

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -7,7 +7,7 @@ const Nav = () => {
     const { user, logOut } = useContext(AuthContext);
     const [theme, setTheme] = useState(localStorage.getItem("theme") ? localStorage.getItem("theme") : "light");
 
-    
+
     const handelToggle = (e) => {
         if (e.target.checked) {
             setTheme("dark");
@@ -99,14 +99,30 @@ const Nav = () => {
             </div>
             {/* <p className='ml-12 text-xl w-96 font-bold'>{moment().format('MMMM Do YYYY, h:mm a')}</p> */}
 
+
             <div className="navbar-end gap-5 mr-5">
-                <div role="button" className="btn btn-ghost btn-circle avatar">
+               
+                <div className="dropdown dropdown-end">
+                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                    <div role="button" className="btn btn-ghost btn-circle avatar">
                     {user && (
                         <div className="w-10 rounded-full">
                             <img alt="?" src={user.photoURL} />
                         </div>
                     )}
                 </div>
+                    </div>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        <li>
+                            <a className="justify-between">
+                                Add food 
+                            </a>
+                        </li>
+                        <li><a>My added food </a></li>
+                        <li><a>My ordered food</a></li>
+                    </ul>
+                </div>
+
                 {
                     user ?
                         <button onClick={handelSignOut} className="btn btn-info text-white font-extrabold w-28">Log Out</button>
@@ -116,10 +132,11 @@ const Nav = () => {
                         </NavLink>
 
                 }
+
                 <label className="swap swap-rotate">
 
                     {/* this hidden checkbox controls the state */}
-                    <input type="checkbox" onChange={handelToggle} checked={theme ==="light" ? false : true} />
+                    <input type="checkbox" onChange={handelToggle} checked={theme === "light" ? false : true} />
 
                     {/* sun icon */}
                     <svg className="swap-off fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" /></svg>
